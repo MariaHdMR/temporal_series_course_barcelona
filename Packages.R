a488315dcb0715a471e339b4af4ad7d5a252b564
mariahu <maria.hurtado.m.r@gail.com>
2024-02-04 20:51:10 +0100

    archivo con los paquetes que se usarán en el curso de series de temporales de bcn

---
+++ Packages.R
@@ -0,0 +1,43 @@
+
+install.packages(c("tidyverse", # data manipulation
+                   "lubridate", # data manipulation
+                   "broom", # data manipulation
+                   "ggplot2", # plots
+                   "ggpubr", # plots
+                   "cowplot", # plots
+                   "MetBrewer", # plots
+                   "RColorBrewer", # plots
+                   "patchwork", # plots
+                   "viridis",# plots
+                   "lattice", # plots
+                   "colorRamps", #plots
+                   "purrr",#plots
+                   "maps", #plots
+                   "mapdata", #plots
+                   "glmmTMB", # regressions
+                   "DHARMa", 
+                   "DT", # tables
+                   "ggrepel", # plots
+                   "codyn", # community dynamics
+                   "vegan", # community 
+                   "eulerr", # Euler diagrams 
+                   "lmtest", #durbin watson test
+                   "imputeTS", #imputation
+                   "miceRanger", #imputation
+                   "naniar", 
+                   "visdat",
+                   "akima",
+                   "timetk",
+                   "TSstudio",
+                   "anomalize", # anomalies
+                   "pillar",
+                   "corrplot", #correlations
+                   "biwavelet", #wavelets
+                   "nlme", # regressions
+                   "lmtest")) # regressions
+
+
+
+
+
+
