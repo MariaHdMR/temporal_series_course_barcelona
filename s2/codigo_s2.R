c9c720eb2d70822172a7b5afaad68e646c904f12
mariahu <maria.hurtado.m.r@gail.com>
2024-02-06 17:52:28 +0100

    codigo sesion 2 temporal series, de dinamicas de poblaciones

--- s2/codigo_s2.R
+++ s2/codigo_s2.R
@@ -135,3 +135,93 @@ p2 <- proj_stoch %>%
 
 p1+ggtitle("Deterministic")|p2+ggtitle("Stochastic")
 
+
+#r exinct function
+
+pop_proj_ext<- function(data, threshold, tmax){ #el treshold lo vamos a poner en 20, que son los que sobreviven
+  n <- tibble(n=0, time=1:tmax)
+  n$n[1] <- 100
+  Ne <- n$n[1]*(1-threshold)
+  for (t in 2:tmax){	
+    # Draw r from normal using estimates of mu and sigma2
+    r <- rnorm(n = 1, mean = mean(data$pop_growth, na.rm=T), 
+               sd = sqrt(var(data$pop_growth, na.rm=T)))
+    # back-transform to get lambda and get population size
+    lambda <- exp(r) 
+    #project one time step from the current population size
+    n$n[t]=n$n[[(t-1)]]*lambda 
+    # leave the loop if the population size is <= threshold (when the threshold is crossed)
+    ifelse(n$n[t] <= Ne, 
+           n$n[t] <- 0, n$n[t] <- n$n[t])
+  }
+  
+  return(n)
+}
+# Apply the quasi-extinction projection function over the populations
+
+proj_extinct <- falco_pops %>%  
+  group_by(id) %>% 
+  do(pop_proj_ext(., threshold = 0.8, tmax=100))
+
+#     r estimte the threshold
+#Estimate the threshold
+Ne <- 100*(1-0.8)
+
+# Which trajectories are above the threshold
+prob <- proj_extinct %>% 
+  group_by(id, time) %>% 
+  mutate(above.qe= n >= Ne,
+         alife=cumprod(above.qe)) %>% #cumprod function transforma a 1 y 0
+  group_by(time) %>% 
+  summarise(prob= 1-(sum(alife, na.rm = T)/length(unique(proj_extinct$id)))) # en 1 menos es la mortalidad, 
+# si no es el 1 - es lo que vive
+
+# Plot it
+prob %>% 
+  ggplot(aes(x=time, y=prob))+
+  geom_line()
+# si la varianza es muy grande, vamos a tener una probabilidad mas grande para morirnos, por eso al final del tiempo
+# la varianza es mayor, por lo que habrá mas variaciones
+
+
+### TASK: 
+# 1. Estimate the quasi-extinction probability at 75 years of the European populations of *Oxyura leucocephala*.
+# 2. Make a plot representing which diets are associated with lower population growth rates.
+
+######
+#TASK 1: Estimate the quasi-extinction probability at 75 years of the European populations of *Oxyura leucocephala*.
+
+oxyura_pops <- bird_all %>%
+  filter(Species_name=="Oxyura leucocephala", Region == "Europe")
+
+proj_extinct_oxy <- oxyura_pops %>%  
+  group_by(id) %>% 
+  do(pop_proj_ext(., threshold = 0.8, tmax=75))
+
+prob_oxy <- proj_extinct_oxy %>% 
+  group_by(id, time) %>% 
+  mutate(above.qe= n >= Ne,
+         alife=cumprod(above.qe)) %>% #cumprod function transforma a 1 y 0
+  group_by(time) %>% 
+  summarise(prob= 1-(sum(alife, na.rm = T)/length(unique(proj_extinct$id)))) # esto es para calcular probabilidad muerte
+
+prob_oxy %>% 
+  ggplot(aes(x=time, y=prob))+
+  geom_line()
+
+prob_oxy %>%  filter(time== 75)
+
+# TASK 2. Make a plot representing which diets are associated with lower population growth rates.
+bird_all$pop_growth
+
+
+ggplot(bird_all, aes(x = pop_growth, y=diet, color= diet))+
+  #facet_wrap(~Species_name)+
+  geom_boxplot(width= 0.1) #☺para hacerlo debería de haber hecho la media
+
+bird_all %>% # esto no se si funciona bien, comprobar
+  mutate(mean.p = mean(pop_growth))
+
+ggplot(bird_all, aes(x = pop_growth, y=diet, color= diet))+
+  #facet_wrap(~Species_name)+
+  geom_point(width= 0.1)
